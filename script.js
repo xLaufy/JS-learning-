570d687274a04fcde99333c47b060f57b039614e
Miłosz Krawczyk <mixol456@gmail.com>
2022-04-15 06:45:42 +0200

    excersises js basics

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
-const pass = 'yomadafaka'
-
-if (pass.length > 10) {
-	console.log('masz dobre hasło')
-} else {
-	console.log('masz za krótkie hasło')
-}
-
 const color = 'blue'
+const newColor = 'green'
 
-if (color == 'blue') {
-	if (10 > 0) {
-		console.log('true true')
-	}
+if (color == newColor) {
+	console.log('Kolory się zgadzają')
 } else {
-	console.log('false')
+	console.log('Kolory się nie zgadzają')
 }
+
+
+
+
+
+
+const x = 100
+const y = 50
+
+if (x > y) {
+	console.log('x>y')
+} else if (x === y) {
+	console.log('x=y')
+} else {
+	console.log('x<y')
+}
+
+
+
+
+
+
+const promo = '20%'
+
+switch (promo) {
+	case '10%':
+		console.log(`Dziś mamy 10% zniżki`)
+		break
+	case '20%':
+		console.log(`Dziś mamy 20% zniżki`)
+		break
+	case '30%':
+		console.log(`Dziś mamy 30% zniżki`)
+		break
+	default:
+		console.log(`Dziś mamy niestandardową opcję`)
+}
+
+
+
+
+
+
+const z = 10
+
+const check = ( z % 2 == 0) ? 'x jest parzyste' : 'x nie jest parzyste'
+
+console.log(check);
+
+
+
+
+const q = 10
+
+if (q >= 100) {
+	text = 'q >= 100';
+} else if (q < 100 && x > 30) {
+	text = 'q jest średniakiem';
+}  else {
+	text = 'q jest mały';
+}
+console.log(text.toUpperCase());

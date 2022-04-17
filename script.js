fcf190bb4ffc62fc27bca6b7a4f4dbceafb3e1f4
Miłosz Krawczyk <mixol456@gmail.com>
2022-04-17 22:26:39 +0200

    js practice

--- script.js
+++ script.js
@@ -1,70 +1,27 @@
-const color = 'blue'
-const newColor = 'green'
+const colors =  ['blue', 'red', 'green', 'black']
 
-if (color == newColor) {
-	console.log('Kolory się zgadzają')
-} else {
-	console.log('Kolory się nie zgadzają')
+for (let i = 0; i  <colors.length; i++){
+    console.log(colors[i]);
+}
+
+let i = 0
+while (i < 5) {
+    console.log(i)
+    i++
 }
 
 
+const cities = ['barcelona','warszawa', 'bialystok', 'madryt']
 
-
-
-
-const x = 100
-const y = 50
-
-if (x > y) {
-	console.log('x>y')
-} else if (x === y) {
-	console.log('x=y')
-} else {
-	console.log('x<y')
+for (let i = 0; i < cities.length; i++) {
+    console.log(`To miasto nazywa sie ${cities[i].toUpperCase()}`);
+    
 }
 
+let x = 0
 
-
-
-
-
-const promo = '20%'
-
-switch (promo) {
-	case '10%':
-		console.log(`Dziś mamy 10% zniżki`)
-		break
-	case '20%':
-		console.log(`Dziś mamy 20% zniżki`)
-		break
-	case '30%':
-		console.log(`Dziś mamy 30% zniżki`)
-		break
-	default:
-		console.log(`Dziś mamy niestandardową opcję`)
+while (x < 10 ) { 
+    x += 2
+    console.log(x);
+    
 }
-
-
-
-
-
-
-const z = 10
-
-const check = ( z % 2 == 0) ? 'x jest parzyste' : 'x nie jest parzyste'
-
-console.log(check);
-
-
-
-
-const q = 10
-
-if (q >= 100) {
-	text = 'q >= 100';
-} else if (q < 100 && x > 30) {
-	text = 'q jest średniakiem';
-}  else {
-	text = 'q jest mały';
-}
-console.log(text.toUpperCase());

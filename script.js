2b821ed0f6f67bd9214bfe43c1c6fc8ef20109bd
Miłosz Krawczyk <mixol456@gmail.com>
2022-09-16 05:37:38 +0200

    dom manipulation exercises

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-// Adding Elements with javascript 
+// Exercise .1
+
+const ul = document.createElement('ul')
+document.body.appendChild(ul)
+
+for (let i = 1; (i <= 10); i++) {
+    const li = document.createElement('li')
+     li.textContent = i
+     ul.appendChild(li)
+    
+}
+
+const lastLi = ul.querySelector('li:last-child')
+
+lastLi.textContent = ('Jestem ostatnim elementem')
+lastLi.style.backgroundColor = 'royalblue', 
+lastLi.style.padding = '20px 40px', 
+lastLi.style.fontSize = '48px';
 
 
-const ulList = document.createElement('ul')
-const liItem = document.createElement('li')
 
-liItem.textContent ='cześć'
+// exercise 2 
 
-document.body.appendChild(ulList)
-ulList.appendChild(liItem)
+const p1  = document.querySelector('.p1')
+const p2  = document.querySelector('.p2')
+const btn1 =document.querySelector('.btn1')
+const btn2 =document.querySelector('.btn2')
+const square = document.querySelector('.square')
 
-const div = document.querySelector('div')
-const p = document.createElement('p')
-div.appendChild(p)
-
-
-
+const text = () => {
+    console.log('Cześć');
+}
 
 
 
+const red = () => {
+    square.style.background ='red'
+}
+const blue = () => {
+    square.style.background ='blue'
+}
+const toggle = () => {
+    p1.classList.toggle('show')
+    p2.classList.toggle('show')
+}
+
+
+btn1.addEventListener('dblclick', text  )
+square.addEventListener('mouseover', red  )
+square.addEventListener('mouseout', blue  )
+btn2.addEventListener('click', toggle  )

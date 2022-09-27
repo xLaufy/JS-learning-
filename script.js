6d559510c18abf0e44b312ad61f8b33b90c0f6ff
Miłosz Krawczyk <mixol456@gmail.com>
2022-09-27 07:50:31 +0200

    js exercises

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-// Exercise .1
+
+// exercises : 
+
+
+const img = document.querySelector('img')
+
+
+img.setAttribute('src', 'https://www.telepolis.pl/images/2022/06/mars-czerwona-planeta-eksploracja-badanie.jpg')
+img.setAttribute('alt', 'mars')
+
+
+// ------------------------------------------
+
+const test = () => {
+    console.log('hey');
+}
+setTimeout(test, 2000)
+
+// druga opcja 
+
+setTimeout(() => {
+	console.log('hey')
+}, '2000')
+
+
+// ---------------------------------------------------
+
+const div = document.createElement('div')
+document.body.appendChild(div)
+div.setAttribute('class', 'wrapper')
 
 const ul = document.createElement('ul')
-document.body.appendChild(ul)
+div.appendChild(ul)
 
-for (let i = 1; (i <= 10); i++) {
+
+
+for ( i=1 ; i <= 5 ; i++){
     const li = document.createElement('li')
-     li.textContent = i
-     ul.appendChild(li)
-    
-}
-
-const lastLi = ul.querySelector('li:last-child')
-
-lastLi.textContent = ('Jestem ostatnim elementem')
-lastLi.style.backgroundColor = 'royalblue', 
-lastLi.style.padding = '20px 40px', 
-lastLi.style.fontSize = '48px';
-
-
-
-// exercise 2 
-
-const p1  = document.querySelector('.p1')
-const p2  = document.querySelector('.p2')
-const btn1 =document.querySelector('.btn1')
-const btn2 =document.querySelector('.btn2')
-const square = document.querySelector('.square')
-
-const text = () => {
-    console.log('Cześć');
+    li.textContent = i
+    ul.appendChild(li)
+    li.dataset.id = i
 }
 
 
-
-const red = () => {
-    square.style.background ='red'
-}
-const blue = () => {
-    square.style.background ='blue'
-}
-const toggle = () => {
-    p1.classList.toggle('show')
-    p2.classList.toggle('show')
-}
+const dataTag = document.querySelector('[data-id="3"]')
 
 
-btn1.addEventListener('dblclick', text  )
-square.addEventListener('mouseover', red  )
-square.addEventListener('mouseout', blue  )
-btn2.addEventListener('click', toggle  )
+console.log(dataTag.closest('.wrapper'));
